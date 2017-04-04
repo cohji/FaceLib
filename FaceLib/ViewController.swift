16f474fd9706a3ecbd5486cc64e26d5dad9566f4
suzuki_k <kohji.suzuki@nts.ricoh.co.jp>
2017-04-04 22:48:48 +0900

    Initial Commit

---
+++ FaceLib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceLib
+//
+//  Created by 鈴木 浩二 on 2017/04/04.
+//  Copyright © 2017年 鈴木 浩二. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
